d184e050a54fde3e6bb09966bc36ea4236964195
Nursurtan <arapovnur23@gmail.com>
2022-05-25 07:34:38 +0700

    Added refactoring with computed properties

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,31 +14,39 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display Label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display Label text to a Double.")
-        }
-        
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
             } else if calcMethod == "%" {
-                displayLabel.text = String(number * 0.01)
+                displayValue *= 0.01
             }
         }
         
-         
-    
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        
         
         //What should happen when a number is entered into the keypad
         
@@ -51,11 +59,7 @@ class ViewController: UIViewController {
                 
                 if numValue == "." {
                     
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        fatalError("Cannot convert display Label text to a Double!")
-                    }
-                    
-                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    let isInt = floor(displayValue) == displayValue
                     
                     if !isInt {
                         return
@@ -70,7 +74,7 @@ class ViewController: UIViewController {
             
         }
         
-    
+        
     }
 }
 
